fc0c0f9fda106c6acb709a38684067edd1213d2d
Sivaprakash Gorantla <sivaprakashgorantla@gmail.com>
2020-07-10 11:52:25 +0530

    first commit

---
+++ springboot-theamleaf/src/main/java/com/gorantla/threamleaf/SpringbootTheamleafApplication.java
@@ -0,0 +1,13 @@
+package com.gorantla.threamleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTheamleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTheamleafApplication.class, args);
+	}
+
+}
